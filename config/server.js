e5fba206d80b8fd6d287a8003290e90db98b9c24
devopsandes <devops@andessalud.ar>
2022-07-20 09:44:41 -0300

    deployment

---
+++ config/server.js
@@ -0,0 +1,9 @@
+module.exports = ({ env }) => ({
+  host: env('HOST', '0.0.0.0'),
+  port: env.int('PORT', 1337),
+  admin: {
+    auth: {
+      secret: env('ADMIN_JWT_SECRET', '5d7616820da94a5760b0eab7055243ee'),
+    },
+  },
+});
---
+++ extensions/users-permissions/config/jwt.js
@@ -0,0 +1,3 @@
+module.exports = {
+  jwtSecret: process.env.JWT_SECRET || '79000210-8415-4993-90d6-4c808a5ecf4f'
+};
